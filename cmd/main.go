9fe0f7e711113d1f479cedfbd60a3ea6b06e44ad
Alexandr <sasha_kobzev18@mail.ru>
2025-08-14 19:30:43 +0300

    feature: server

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,55 @@ package main
 
 import (
 	"UrlShortener/internal/config"
-	"UrlShortener/internal/logger"
+	"UrlShortener/internal/http-server/handlers/url/save"
+	"UrlShortener/internal/http-server/middleware/logger"
+	"UrlShortener/internal/logger/sl"
+	"UrlShortener/internal/storage/postgresql"
 	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
 	cfg := config.Load()
 
-	log := logger.SetupLogger(cfg.Env)
+	log := sl.SetupLogger(cfg.Env)
 
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
+
+	storage, err := postgresql.Init(cfg.PosgreSQL.User, cfg.PosgreSQL.Password, cfg.PosgreSQL.Name)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(logger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Post("/url", save.New(log, storage))
+
+	log.Info("starting server", slog.String("addres", cfg.HTTPServer.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
 }
