743a29d3c36f0847385a8cc61009e6a14427efc8
sasha <sasha_kobzev18@mail.ru>
2025-10-02 17:46:40 +0300

    chore: removing unnecessary fields

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,15 +13,13 @@ type Config struct {
 	HTTPServer HTTPServer    `yaml:"http_server"`
 	PosgreSQL  PosgreSQL     `yaml:"postgres"`
 	Clients    ClientsConfig `yaml:"clients"`
-	AppSecret  string        `yaml:"app_secret" env-required:"true" env"APP_SECRET"`
+	AppSecret  string        `yaml:"app_secret" env-required:"true" env:"APP_SECRET"`
 }
 
 type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
-	User        string        `yaml:"user" env-required:"true"`
-	Password    string        `yaml:"password" env-required:"true" env:"HTTP_SERVER_PASSWORD"`
 }
 
 type PosgreSQL struct {
